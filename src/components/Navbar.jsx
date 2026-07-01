934418f43540459fee1a16846773da31708fae9f
FiliDalzi <fili.dalzi@gmail.com>
2026-07-01 23:33:23 +0200

    Initial commit - clean code

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import dayjs from "dayjs";
+import 'dayjs/locale/it.js';
+import { navIcons, navLinks } from "#constants";
+
+dayjs.locale("it");
+
+const Navbar = () => {
+  return (
+  <nav>
+    <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Filippo's Portfolio</p>
+
+        <ul>
+            {navLinks.map(({ id, name }) => (
+                <li key={id}>
+                    <p>{name}</p>
+                </li>
+            ))}
+        </ul>
+    </div>
+
+    <div>
+        <ul>
+            {navIcons.map(({ id,img }) => (
+                <li key={id}>
+                    <img src={img} className="icon-hover" alt={`icon-${id}`} />
+                </li>
+            ))}
+        </ul>
+
+        <time>{dayjs().format("ddd D MMM h:mm A")}</time>
+    </div>
+  </nav>
+  );
+};
+
+export default Navbar;
